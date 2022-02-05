283ecf935f66a90a5c5bdd29386e48affbc9690a
jaeha13 <86695215+jaeha13@users.noreply.github.com>
2022-02-05 12:36:08 +0900

    Update 타겟 넘버.py
    수정

--- programmers/DFS,BFS/타겟 넘버.py
+++ programmers/DFS,BFS/타겟 넘버.py
@@ -4,23 +4,13 @@ def solution(numbers, target):
     def dfs(num, level):
         nonlocal answer
 
-        if level == len(numbers) - 1:
+        if level == len(numbers):
             if num == target:
                 answer += 1
             return
 
-        if not level:
-            dfs(-num + numbers[level], level + 1)
-            dfs(-num - numbers[level], level + 1)
-            dfs(num + numbers[level], level + 1)
-            dfs(num - numbers[level], level + 1)
+        dfs(num + numbers[level], level + 1)
+        dfs(num - numbers[level], level + 1)
 
-        else:
-            dfs(num + numbers[level], level + 1)
-            dfs(num - numbers[level], level + 1)
-
-    dfs(numbers[0], 0)
+    dfs(0, 0)
     return answer
-
-
-print(solution([1, 1, 1, 1, 1],	3))
